cdd13f988fa6b41e31fda58ec278f9ebddcc6f07
SeanDuffie <sean.duffie@tamu.edu>
2024-03-21 01:30:49 -0400

    Added timeline structure example from pascal

--- script/script.js
+++ script/script.js
@@ -10,3 +10,73 @@ window.onscroll = function() {
         header.classList.remove('navbarDark');
     }
 }
+
+// Co = [{
+//     from: "June 2023",
+//     to: null,
+//     location: "Eindhoven",
+//     url: "https://returnless.com",
+//     type: null,
+//     employment: "fulltime",
+//     role: "Lead Developer",
+//     company: "Returnless",
+//     description: "As the Lead Developer at Returnless, me and the team are developing an all-in-one Return Platform. Using Laravel combined with Vapor and a beautiful Vue3 / Tailwind front-end we deliver a sweatless return experience for our customers."
+// }, {
+//     from: "June 2022",
+//     to: "May 2023",
+//     location: "Eindhoven",
+//     url: "https://wolfpackit.nl",
+//     type: null,
+//     employment: "fulltime",
+//     role: "Lead Developer",
+//     company: "Wolfpack",
+//     description: "As a Lead Developer at Wolfpack, I was working on a variety of projects all within the healthcare, education, and innovation sectors. With my past experiences I was committed of helping the company achieve an even greater success. Not only with development, but also by improving the way we're working."
+// }, {
+//     from: "May 2016",
+//     to: "May 2022",
+//     location: "Eindhoven",
+//     url: "https://startselect.com",
+//     type: null,
+//     employment: "fulltime",
+//     role: "Senior Developer",
+//     company: "Startselect",
+//     description: "At Startselect, I was a Fulltime Senior Developer working on a large, global webshop for digital games, gift cards, and other digital entertainment. The application was based on Laravel, and as a team, we followed the Scrum methodology for our work. As a senior, I provided assistance to various departments within the company to help them achieve their goals."
+// }],
+// To = [{
+//     from: "Mar 2014",
+//     to: "May 2014",
+//     location: "Penn University",
+//     url: null,
+//     type: null,
+//     employment: "online",
+//     role: "Course",
+//     company: "Gamification",
+//     description: "I successfully completed a course on gamification, learning how to incorporate gaming elements and techniques into my daily work. I also enjoyed the close connection with UX that this approach brings."
+// }, {
+//     from: "Mar 2008",
+//     to: "May 2004",
+//     location: "'s-Hertogenbosch",
+//     url: null,
+//     type: "Minor: Multimedia",
+//     employment: "bachelor",
+//     role: "Computer Science",
+//     company: "Avans",
+//     description: "I graduated from my bachelor's degree with excellent grades. I started off with Java and gained a thorough understanding of algorithms. I also completed a minor in web and game development (multimedia)."
+// }],
+// Is = {
+//     careers: Co,
+//     educations: To
+// },
+// Po = Pe({
+//     __name: "Experiences",
+//     setup(e) {
+//         var s, l;
+//         const t = (s = Is.careers) != null ? s : [],
+//             n = (l = Is.educations) != null ? l : [];
+//         return (r, i) => (j(), W(q, null, [O("section", Ao, [O("div", Eo, [Io, (j(!0), W(q, null, Qe(xe(t), c => (j(), Et(Es, {
+//             experience: c
+//         }, null, 8, ["experience"]))), 256))])]), O("section", So, [O("div", Oo, [Mo, (j(!0), W(q, null, Qe(xe(n), c => (j(), Et(Es, {
+//             experience: c
+//         }, null, 8, ["experience"]))), 256))])])], 64))
+//     }
+// })
